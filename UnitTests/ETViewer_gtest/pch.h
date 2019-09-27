f3b820ffa1de722551d538ce57534d7b669ac3da
Chris Conti <cmconti@users.noreply.github.com>
2019-09-27 10:40:12 -0400

    google test is part of VS2017.  Remove local copy and cleanup

---
+++ UnitTests/ETViewer_gtest/pch.h
@@ -0,0 +1,18 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+
+#include <atlbase.h>
+#include <atlstr.h>
+
+
+#include "gtest/gtest.h"
